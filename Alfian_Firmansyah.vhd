710623aebce4eb2569eb994e3a211188a02447bb
Alfian Firmansyah <36856722+bloodberrys@users.noreply.github.com>
2019-02-28 10:17:38 +0700

    Update Alfian_Firmansyah.vhd

--- Alfian_Firmansyah.vhd
+++ Alfian_Firmansyah.vhd
@@ -6,14 +6,14 @@ entity circuit is
 port (
 	A, B, C, D : in std_logic_vector(3 downto 0);
 	SEL :in std_logic_vector(3 downto 0);
-	O : out std_logic_vector(3 downto 0);
+	Output : out std_logic_vector(3 downto 0);
 	BCD : in std_logic_vector (3 downto 0);
 	Seven7 : out std_logic_vector (6 downto 0));
 end circuit;
 
 architecture mux4 of circuit is
 begin
-	O <= A when (SEL = "0011") else
+	Output <= A when (SEL = "0011") else
 	     B when (SEL = "0010") else
              C when (SEL = "0001") else
 	     D when (SEL = "0000") else
