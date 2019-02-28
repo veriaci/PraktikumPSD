8289de8997604b7b4b437fbab5d2183aeda57e67
samuel9914 <36689849+samuel9914@users.noreply.github.com>
2019-02-28 13:03:01 +0700

    Update Samuel_1706986076_multidecod7seg.vhd

--- Samuel_1706986076_multidecod7seg.vhd
+++ Samuel_1706986076_multidecod7seg.vhd
@@ -1,12 +1,48 @@
-entity  helloworld1 is 
-end entity;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.all;
 
+entity decoder7seg is 
+	port ( 	SEL1 : in  STD_LOGIC;
+		SEL2 : in STD_logic;
+           A   : in  STD_LOGIC_VECTOR (3 downto 0);
+           B   : in  STD_LOGIC_VECTOR (3 downto 0);
+	   C 	: in STD_LOGIC_VECTOR (3 downto 0);
+		D : in STD_logic_vector (3 downto 0);
+           X   : out STD_LOGIC_VECTOR (3 downto 0);
 
-architecture sim of helloworld1 is 
+		reset : in std_logic;
+		
+		out7seg : out std_logic_vector  (6 downto 0));
+end decoder7seg;
+
+architecture decoder7seg of decoder7seg is
+
+signal indec: std_logic_vector (3 downto 0);
 begin
-	process is 
-	begin 
-		report "HELLO WORLD";
-		wait;
-	end process;
-end architecture;
+
+			indec <= A when (SEL1 = '0' and SEL2= '0') else
+			 B when (SEL1 = '0' and SEL2= '1') else 
+			C when (SEL1 = '1' and SEL2= '0') else
+			D when (SEL1 = '1' and SEL2= '1');
+			
+			
+			
+	
+		
+			out7seg <= "0000001" when indec="0000" else
+			 	"1001111"  when indec="0001" else
+			 	"0010010" when indec="0010" else
+				"0000110"  when indec="0011" else
+				"1001100"  when indec="0100" else
+				"0100100" when indec="0101" else
+				"0100000" when indec="0110" else
+				"0001111"  when indec="0111" else
+				"0000000" when indec="1000" else
+				"0100100" when indec="1001" else
+				 "1111111";
+		
+	
+
+
+end decoder7seg;
